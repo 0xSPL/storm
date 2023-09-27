efda62cc28c8e739419af8237797cf237a2bff97
0xSPL <user@example.com>
2023-09-27 09:15:17 -0700

    Add signature consts

---
+++ crates/storm-core/src/consts.rs
@@ -0,0 +1,13 @@
+// =============================================================================
+// Signatures
+// =============================================================================
+
+pub const MAGIC_ID: [u8; 4] = *b"MPQ\x1A";
+
+pub const MAGIC_UD: [u8; 4] = *b"MPQ\x1B";
+
+pub const MAGIC_HET: [u8; 4] = *b"HET\x1A";
+
+pub const MAGIC_BET: [u8; 4] = *b"BET\x1A";
+
+pub const MAGIC_SIGN: [u8; 4] = *b"NGIS";
---
+++ crates/storm-core/src/lib.rs
@@ -0,0 +1,5 @@
+//! Core tools for working with MPQ archives.
+
+#![no_std]
+
+pub mod consts;
